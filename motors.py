eb45e0645cdfc976e3fe8a4cdd47dea3a381dc96
sartamo <sartamo@e.email>
2025-05-26 16:12:06 +0200

    motors.py

---
+++ motors.py
@@ -0,0 +1,28 @@
+from gpiozero import Motor
+from time import sleep
+import logging
+
+class Wheel:
+    def __init__(self, position, ports): 
+        self.position = position # Position is a tuple (x, y): can be each 0 or 1, (0, 0) is back left, (1, 1) is front right
+        self.motor = Motor(ports[0], ports[1])
+        self.speed = 0 # Between -1 and 1, where -1 is full reverse, 0 is stop, and 1 is full forward
+
+    def setspeed(self):
+        if self.speed > 0:
+            self.motor.forward(self.speed)
+        elif self.speed < 0:
+            self.motor.backward(-self.speed)
+        else:
+            self.motor.stop()
+        logging.info(f"Motor speed set to {self.speed} for motor at {self.position}")
+
+wheel = Wheel((0, 0), (2, 3))
+
+wheel.speed = 1
+wheel.setspeed()
+while True:
+    sleep(5)
+    wheel.speed *= -1
+    wheel.setspeed()
+    print("Motor reversed")
