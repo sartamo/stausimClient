1212cb97957f46cd63637e521558fe6276be7129
sartamo <sartamo@e.email>
2026-01-03 00:18:34 +0100

    mecanum drive

--- motors.py
+++ motors.py
@@ -2,6 +2,18 @@ from gpiozero import Motor
 from time import sleep
 import logging
 import configparser
+import math
+
+def mecanumSpeeds(angle, magnitude):
+    assert magnitude <= 1 and magnitude >= 0 # speeds between 0 and 1 allowed
+
+    rf = math.sin(angle - math.pi / 4) * magnitude
+    lb = rf
+
+    lf = math.sin(angle + math.pi / 4) * magnitude
+    rb = lf
+
+    return lf, rf, lb, rb
 
 class Wheel:
     def __init__(self, port1, port2): 
@@ -10,14 +22,15 @@ class Wheel:
         self.port2 = port2
         self.speed = 0 # Between -1 and 1: -1 full reverse, 0 stop, and 1 full forward
 
-    def setspeed(self):
+    def setspeed(self, speed):
+        self.speed = speed
         if self.speed > 0:
             self.motor.forward(self.speed)
         elif self.speed < 0:
             self.motor.backward(-self.speed)
         else:
             self.motor.stop()
-        print(f"Motor speed set to {self.speed} at ports {self.port1}, {self.port2}")
+        logging.info(f"Motor speed set to {self.speed} at ports {self.port1}, {self.port2}")
 
 config = configparser.ConfigParser()
 config.read('config.ini')
@@ -28,7 +41,16 @@ wheellf = Wheel(config.getint('leftfront', 'pin1'), config.getint('leftfront', '
 wheelrf = Wheel(config.getint('rightfront', 'pin1'), config.getint('rightfront', 'pin2'))
 
 if __name__ == '__main__':
-    wheellf.speed = 1
-    wheellf.setspeed()
+    format = "%(asctime)s: %(message)s"
+    logging.basicConfig(format=format, level=logging.INFO, datefmt="%H:%M:%S")
+
+    angle = 0
     while True:
-        pass
+        sleep(0.1)
+        angle += 0.1
+        lf, rf, lb, rb = mecanumSpeeds(angle, 0.1)
+        wheellb.setspeed(lb)
+        wheelrb.setspeed(rb)
+        wheellf.setspeed(lf)
+        wheelrf.setspeed(rf)
+
