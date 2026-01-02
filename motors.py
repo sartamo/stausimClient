45d436a45353d39d055a32426bc379e0fcb5e0a2
sartamo <sartamo@e.email>
2026-01-02 23:49:34 +0100

    updated

--- motors.py
+++ motors.py
@@ -6,6 +6,8 @@ import configparser
 class Wheel:
     def __init__(self, port1, port2): 
         self.motor = Motor(port1, port2)
+        self.port1 = port1
+        self.port2 = port2
         self.speed = 0 # Between -1 and 1: -1 full reverse, 0 stop, and 1 full forward
 
     def setspeed(self):
@@ -15,7 +17,7 @@ class Wheel:
             self.motor.backward(-self.speed)
         else:
             self.motor.stop()
-        logging.info(f"Motor speed set to {self.speed} for motor at {self.position}")
+        print(f"Motor speed set to {self.speed} at ports {self.port1}, {self.port2}")
 
 config = configparser.ConfigParser()
 config.read('config.ini')
@@ -26,4 +28,7 @@ wheellf = Wheel(config.getint('leftfront', 'pin1'), config.getint('leftfront', '
 wheelrf = Wheel(config.getint('rightfront', 'pin1'), config.getint('rightfront', 'pin2'))
 
 if __name__ == '__main__':
-    wheelrb.setspeed(1)
+    wheellf.speed = 1
+    wheellf.setspeed()
+    while True:
+        pass
