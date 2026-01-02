4cf7c9ac8ca160455f498dfafab7e13c3d70dcbb
sartamo <sartamo@e.email>
2026-01-02 14:09:43 +0100

    modified motors.py

--- motors.py
+++ motors.py
@@ -15,15 +15,30 @@ class Wheel:
             self.motor.backward(-self.speed)
         else:
             self.motor.stop()
-        logging.info(f"Motor speed set to {self.speed} for motor at {self.position}")
+        print(f"Motor speed set to {self.speed} for motor at {self.position}")
 
-wheel = Wheel((0, 0), (2, 3))
+wheel1 = Wheel((0, 0), (2, 3))
+wheel2 = Wheel((0, 1), (4, 14))
+wheel3 = Wheel((1, 0), (22, 23))
+wheel4 = Wheel((1, 1), (27, 17))
 
-wheel.speed = 0
-wheel.setspeed()
+wheel1.speed = 0
+wheel1.setspeed()
+wheel2.speed = 0
+wheel2.setspeed()
+wheel3.speed = 0
+wheel3.setspeed()
+wheel4.speed = 0
+wheel4.setspeed()
 while True:
     sleep(0.5)
-    if wheel.speed <= 0.95:
-        wheel.speed += 0.05
-        wheel.setspeed()
-        print(f"Motor speed set to {wheel.speed}")
+    if wheel1.speed <= 0.95:
+        print(f"speed increased to {wheel1.speed}")
+        wheel1.speed += 0.05
+        wheel1.setspeed()
+        wheel2.speed += 0.05
+        wheel2.setspeed()
+        wheel3.speed += 0.05
+        wheel3.setspeed()
+        wheel4.speed += 0.05
+        wheel4.setspeed()
